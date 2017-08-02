b5d2540f707a4148bf8193bdf0bf14fb47a07e53
valeriomoroni95 <valeriomoroni95@gmail.com>
2017-08-02 16:01:06 +0200

    createtd some classes and crontroller

---
+++ src/boundary/LoginBean.java
@@ -0,0 +1,91 @@
+package boundary;
+
+import entity.Utente;
+
+public class LoginBean {
+	private String username;
+	private String password;
+	private String nome;
+	private String cognome;
+	private Utente found;
+	private String email;
+	private boolean isadmin;
+
+	public LoginBean() {
+		this.username = "";
+		this.password = "";
+	}
+	
+	public void removeFound(){
+		this.found = null;
+	}
+
+	public void setUsername(String user) {
+		this.username = user;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setPassword(String pwd) {
+		this.password = pwd;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setCognome(String cogn) {
+		this.cognome = cogn;
+	}
+
+	public String getCognome() {
+		return this.cognome;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public boolean getAdmin() {
+		return this.isadmin;
+	}
+
+	public boolean validate() {
+		// Controllo sintattico
+		boolean res1=true,res2=false;
+		if (this.username.equals("") || this.password.equals("")) {
+			res1=false;
+		}
+		
+		
+		LoginController controller = LoginController.getInstance(this);
+		found = controller.login(this.username, this.password);
+		if(found != null) {
+			this.nome = found.getNome();
+			this.cognome = found.getCognome();
+			res2 = true;
+		}
+		
+		if(res1==true && res2==true)
+			return true;
+		return false;
+	}
+	
+}
+
+	
+
---
+++ src/entity/Utente.java
@@ -0,0 +1,8 @@
+package entity;
+
+public interface Utente {
+	public String getNome();
+	public String getCognome();
+	public int getTipo();
+	public String getUsername();
+}
