655f5d74405e8d4972da37b7e094191abc99a26c
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-08-04 12:48:55 +0200

    create agency entity

---
+++ src/entity/Agency.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class Agency {
+
+	private int agencyId;
+	private String agencyName;
+	
+	public Agency() {
+		
+	}
+	
+	public int getAgencyId() {
+		return agencyId;
+	}
+	
+	public String getAgencyName() {
+		return agencyName;
+	}
+	
+}
