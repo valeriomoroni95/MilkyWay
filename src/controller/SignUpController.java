630af6d7efbfda0ac7afc5942a32e0cdeb62eb64
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-08-06 21:19:05 +0200

    new method in controller

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -12,5 +12,12 @@ public class SignUpController {
 	
 	private SignUpController() {
 		
-	}	
+	}
+	
+	public boolean SignUp(String username, String password, String name, String surname, String email, Boolean isadmin ) {
+		
+		//this need to be completed, I wrote the return just to avoid the error inside SignUpBean
+		return true;
+		
+	}
 }
