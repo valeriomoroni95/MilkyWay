333b0a9f91f709add19e00edef55d2e3f8c1a1f5
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-08-06 20:25:44 +0200

    made the user dao like the professor file example and optimized a conditional statement

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -14,64 +14,91 @@ import entity.User;
 
 public class UserDao {
 
-	private static DataSource dataSource;
-	
-	//Constructor
-	public UserDao(){
-		dataSource = new DataSource();
-	}
-	
-	public static User findByNameAndPassword (String username, String password) throws SQLException {
-		Connection connection = null;
-		PreparedStatement statement = null;
-		User u = null;
-		try {
-			String select = "SELECT user_id, name, surname, is_admin, mail password FROM user  where user_id = '"
-                    + username + "' AND password = '" + password + "';";
-			connection = dataSource.getConnection();
-			statement = connection.prepareStatement(select);
-			ResultSet rs = statement.executeQuery();
-			if (rs == null)
-				return null;
-			else {
-				String name = rs.getString("name");
-	            String surname = rs.getString("surname");
-	            String usernameLoaded = rs.getString("user_id");
-	            String emailLoaded = rs.getString("mail");
-	            boolean is_admin = rs.getBoolean("is_admin");
-	            
-	            System.out.println(is_admin);
-	            
-	            if(is_admin == false)
-	            	u = new CommonUser(usernameLoaded, "", name, surname, emailLoaded);
-	            if(is_admin == true)
-	            	u = new AdminUser(usernameLoaded, "", name, surname, emailLoaded);
-	            
-	            rs.close();
-	            statement.close();
-	            connection.close();
+    private static DataSource dataSource;
 
-		}
-		
-				
-		}catch (SQLException se) {
-			se.printStackTrace();
-		}catch (Exception e){
-			e.printStackTrace();
-		}finally {
-            try {
-                if (statement != null)
-                    statement.close();
-            } catch (SQLException se2) {
+    //Constructor
+    public UserDao() {
+
+        dataSource = new DataSource();
+
+    }
+
+    public static User findByUsernameAndPassword(String username, String password) throws SQLException {
+
+        Connection connection = null;
+
+        PreparedStatement statement = null;
+
+        User user = null;
+
+        ResultSet result = null;
+
+        final String query = "SELECT user_id, name, surname, is_admin, mail password FROM user WHERE user_id = '"
+                + username + "' AND password = '" + password + "';";
+
+        try {
+
+            connection = dataSource.getConnection();
+
+            statement = connection.prepareStatement(query);
+
+            result = statement.executeQuery();
+
+            if (result == null) {
+
+                return null;
+
+            }else {
+
+                String name = result.getString("name");
+
+                String surname = result.getString("surname");
+
+                String usernameLoaded = result.getString("user_id");
+
+                String emailLoaded = result.getString("mail");
+
+                boolean is_admin = result.getBoolean("is_admin");
+
+                System.out.println(is_admin);
+
+                if (!is_admin) {
+
+                    user = new CommonUser(usernameLoaded, "", name, surname, emailLoaded);
+
+                } else {
+
+                    user = new AdminUser(usernameLoaded, "", name, surname, emailLoaded);
+
+                }
+
+                result.close();
+
+                statement.close();
+
+                connection.close();
+
             }
-            try {
-                if (connection != null)
-                    connection.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            if (result != null) {
+                result.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
             }
         }
-		return u;
-	}
+        return user;
+    }
 }
 
