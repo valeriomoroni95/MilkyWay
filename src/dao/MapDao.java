810b94dac93cc69c459f37612a196eaeb6962ad4
Unknown <luca.messina@live.it>
2017-09-05 23:46:34 +0200

    Added inutile incompleto metodo di Valerio to MapDao

--- src/dao/MapDao.java
+++ src/dao/MapDao.java
@@ -6,14 +6,88 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import entity.Map;
+
 public class MapDao {
 	
     private static DataSource dataSource;
+    
+    public static Map[][] showMaps() throws SQLException {
+        
+        Connection connection = null;
+        Statement statement = null;
+        Map[][] maps = {};    
+        ResultSet result = null;
+         
+        final String query = "SELECT * FROM \"map\";"; //voglio selezionare id mappa e nome mappa per tutte le mappe
+            
+        try {                
+        	DataSource d = new DataSource();
+        	connection = d.getConnection();                
+        	statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            result = statement.executeQuery(query);
+                
+            if (!result.first()) {
+                  return null;
+            }
+            
+            return maps;
+      }
+}
+      
     
     public static boolean addMap(String mapId, String mapName) {
    	 
     	Connection connection = null;
         Statement statement = null;
-                
-    }   
-}
+        ResultSet result = null;
+        
+        final String query = "SELECT \"map_id\" FROM \"map\" WHERE \"map_id\" = '"+mapId+"';";
+        try {
+       	 	DataSource d = new DataSource();
+            connection = d.getConnection();
+            System.out.println("MapDao.java: addMap.java: sto richiedendo una connessione" + connection);
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            System.out.println("MapDao.java: addMap.java: ho preparato una statement" + statement);
+            result = statement.executeQuery(query);
+            System.out.println("MapDao.java: addMap.java: ho un result" + result);
+            
+            if (result == null) {
+           	 System.out.println("MapDao.java: errore nel primo if");
+                return false;
+            }
+            
+            if(result.first())
+           	 return false;
+            
+            else {
+                String sql1 = "INSERT INTO \"map\"(map_id, name) VALUES ('"+mapId+"','"+mapName+"');";
+                System.out.println("ho fatto la insert " + sql1);
+                statement.executeUpdate(sql1);
+            }
+            result.close();
+            statement.close();
+            connection.close();
+            
+        } catch (SQLException se) {
+            // Errore durante l'apertura della connessione
+            se.printStackTrace();
+        } catch (Exception e) {
+            // Errore nel loading del driver
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null)
+                    statement.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return true;
+    }
+}   
