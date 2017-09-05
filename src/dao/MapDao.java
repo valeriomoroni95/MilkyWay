7ce1e223ad332e3277b161c7fe153262e06569c9
Unknown <luca.messina@live.it>
2017-09-05 22:55:36 +0200

    Added MapDao

---
+++ src/dao/MapDao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MapDao {
+	
+    private static DataSource dataSource;
+    
+    public static boolean addMap(String mapId, String mapName) {
+   	 
+    	Connection connection = null;
+        Statement statement = null;
+                
+    }   
+}
