bb22c7c89275c6cd0e9fb09717344e49df2b0034
Unknown <luca.messina@live.it>
2017-09-06 13:13:34 +0200

    Updated Satellite, SatelliteDao, created SatelliteBean

--- src/entity/Satellite.java
+++ src/entity/Satellite.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.util.List;
+
 //import java.sql.Date;
 
 public class Satellite {
@@ -9,11 +11,24 @@ public class Satellite {
 	//private Date satelliteEnd;
 	private String satelliteStart;
 	private String satelliteEnd;
-	public Satellite(int satelliteId, String satelliteName, String satelliteStart, String satelliteEnd) {
+	private List<String> tools;
+	private String duration;
+	
+	public Satellite(int satelliteId, String satelliteName, String satelliteStart, String satelliteEnd, List<String> tools) {
 		this.satelliteId = satelliteId;
 		this.satelliteName = satelliteName;
 		this.satelliteStart = satelliteStart;
 		this.satelliteEnd = satelliteEnd;
+		this.tools = tools;
+		if(satelliteEnd!=null)
+			this.duration = calculateDuration();
+	}
+	
+	public List<String> getTools() {
+		return this.tools;
+	}
+	public void setTools(List<String> tools) {
+		this.tools = tools;
 	}
 	
 	public int getSatelliteId() {
@@ -44,23 +59,26 @@ public class Satellite {
 		this.satelliteEnd = satelliteEnd;
 	}
 	
+	public String calculateDuration() {
+		String[] x = this.satelliteStart.split("/");
+		String[] y = this.satelliteEnd.split("/");
+		int startYear = Integer.parseInt(x[2]);
+		int endYear = Integer.parseInt(y[2]);
+		int startMonth = Integer.parseInt(x[1]);
+		int endMonth = Integer.parseInt(y[1]);
+		int startDay = Integer.parseInt(x[0]);
+		int endDay = Integer.parseInt(y[0]);
+		int months;
+		int years;
+		int days;
+		
+		days = (startYear-endYear)*365 + (startMonth-endMonth)*30 + startDay - endDay;
+		years = days/365;
+		months = (days-years*365)/30;
+		days = days - years*365 - months*30;
+		String k = "Il satellite è attivo da " + Integer.toString(years) + 
+				" anni, " + Integer.toString(months) + " mesi e" + Integer.toString(days) + "giorni.";
+		System.out.println(k);
+		return k;
+	}
 }
-/* CONVERSIONE DATA FORMATO gg/mm/aaaa per calcolare DurataOperazione:
- * String[] x = satelliteStart.split("/");
- * String[] y = satelliteEnd.split("/");
- * int startYear = parseInt(x[2]);
- * int endYear = parseInt(y[2]);
- * int startMonth = parseInt(x[1]);
- * int endMonth = parseInt(y[1]);
- * int months;
- * int years;
- * if (endMonth > startMonth){
- * 		months = endMonth - startMonth;
- * 		years = endYear - startYear;
- * }
- * else{
- * 		months = 12 - endMonth - startMonth;
- * 		years = endYear - startYear - 1;
- * }
- * System.out.println("Il satellite è attivo da " + years + " anni e " + months + " mesi.");
- * */
