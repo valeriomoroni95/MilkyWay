fb8acf3b6674487e8f3c91c0cecb8c33d3575afb
valeriomoroni95 <valeriomoroni95@gmail.com>
2017-09-10 06:49:22 +0200

    SEEE PORCODDIO I FILE CHE FUNZIONANO PORCODDIO (higal.csv)

--- src/dao/ClumpDao.java
+++ src/dao/ClumpDao.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 import java.util.Vector;
 
 public class ClumpDao {
@@ -208,6 +209,61 @@ public class ClumpDao {
 		    	}
 		 	 return v;
 	}
+//    for(String[] rawClumpData : fileData){
+
+	public boolean isPresent(int clumpId) {
+		Connection connection = null;
+        PreparedStatement statement = null;
+           	
+        ResultSet result = null;
+        	
+        
+        try {
+       	 	
+        	System.out.println("ClumpDao: isPresent.java: sto dopo il try");
+        	final String query = "SELECT \"clump_id\" FROM \"clump\" WHERE \"clump_id\" = ?;";
+        	DataSource d = new DataSource();
+        	System.out.println("ClumpDao: isPresent.java: sopravvissuto al Datasource");
+
+            connection = d.getConnection();
+            
+            PreparedStatement pStatement = connection.prepareStatement(query,ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            pStatement.setInt(1, clumpId);
+            result = pStatement.executeQuery();
+            System.out.println("ClumpDao: isPresent.java: ho eseguito la query");
+
+            
+            
+            if(result.first())
+           	 return true;
+          
+            result.close();
+            pStatement.close();
+            connection.close();
+            return false;  
+        } catch (SQLException se) {
+            // Errore durante l'apertura della connessione
+            se.printStackTrace();
+        } catch (Exception e) {
+            // Errore nel loading del driver
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null)
+                    statement.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return true;
+    }
+		
+	
 	
 	
 	public Vector<String[]> showClumpsInArea(Double latitude,Double longitude, Double lenght, boolean isCircle){
--- src/dao/FileImportDao.java
+++ src/dao/FileImportDao.java
@@ -6,28 +6,73 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Vector;
 
 
 public class FileImportDao {
 	
-public boolean importSatellite(String filename) {
+public boolean importSatellite(String filename) throws ClassNotFoundException, SQLException {
     	
     	Connection connection = null;
 		PreparedStatement statement = null;
-		
+		ClumpDao cd = new ClumpDao();
 		String line = "";
         String csvSplitBy = ",";
-
+        int rowIndex = 0;
+        
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-
+        	
+        	DataSource d = new DataSource();
+            connection = d.getConnection();
+            final String query = "INSERT INTO \"clump\"(clump_id ,g_lon, g_lat, k_temp, ratio, surf_dens, c_type, map_id) VALUES (?,?,?,?,?,?,?,3)";
+            statement = connection.prepareStatement(query);
+            int clumpId;
+            double gLon;
+            double gLat;
+            double temp;
+            double ratio;
+            double surfDens;
+            int cType;
+            
             while ((line = br.readLine()) != null) {
 
-                String[] vect = line.split(csvSplitBy);
-
-                System.out.println(vect[0]);
-
-            }
-        } catch (FileNotFoundException e) {
+            	if(rowIndex++ > 11){
+            		
+            		String[] vect = line.split(csvSplitBy);
+            		clumpId = Integer.parseInt(vect[0]);
+            		gLon = Double.parseDouble(vect[1]);
+            		gLat = Double.parseDouble(vect[2]);
+            		temp = Double.parseDouble(vect[3]);
+            		ratio = Double.parseDouble(vect[4]);
+            		surfDens = Double.parseDouble(vect[5]);
+            		cType = Integer.parseInt(vect[6]);
+            		if(cd.isPresent(clumpId)){
+            			String sql1 = "UPDATE clump SET(clump_id ,g_lon, g_lat, k_temp, ratio, surf_dens, c_type, map_id) = (?,?,?,?,?,?,?,?,3);";
+            			statement.setInt(1, clumpId);
+                        statement.setDouble(2, gLon);
+            			statement.setDouble(3, gLat);
+            			statement.setDouble(4, temp);
+            			statement.setDouble(5, ratio);
+            			statement.setDouble(6, surfDens);
+            			statement.setInt(7, cType);
+            			statement.executeUpdate();
+            		}
+            		else{
+            			String sql2 = "INSERT INTO clump VALUES(clump_id ,g_lon, g_lat, k_temp, ratio, surf_dens, c_type, map_id) VALUES (?,?,?,?,?,?,?,3);";
+            			statement.setInt(1, clumpId);
+                        statement.setDouble(2, gLon);
+            			statement.setDouble(3, gLat);
+            			statement.setDouble(4, temp);
+            			statement.setDouble(5, ratio);
+            			statement.setDouble(6, surfDens);
+            			statement.setInt(7, cType);
+            			statement.executeUpdate();
+            		}
+            	}
+            	}
+            }	 catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -35,11 +80,11 @@ public boolean importSatellite(String filename) {
         return true;
     }
 
-public static void main(String args[]) {
+public static void main(String args[]) throws ClassNotFoundException, SQLException {
 	FileImportDao dao = new FileImportDao();
-	dao.importSatellite("/Users/Valerio/Desktop/Basi di Dati e Conoscenza/PROG_DB_2016_2017higal.csv");
+	dao.importSatellite("/Users/Valerio/Desktop/higal.csv");
 		
 }
 
 }
-    
+       
