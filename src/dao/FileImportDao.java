60c91af08585bc5e0e3fcaaa1dadaf823bb46cc8
Unknown <luca.messina@live.it>
2017-09-11 04:09:47 +0200

    Insert flux insert and update

--- src/dao/FileImportDao.java
+++ src/dao/FileImportDao.java
@@ -89,6 +89,8 @@ public boolean importSource(String filename) throws ClassNotFoundException, SQLE
 	Connection connection = null;
 	Statement statement1 = null;
 	PreparedStatement pstatement = null;
+	PreparedStatement p2statement = null;
+	PreparedStatement p3statement = null;
 	SourceDao sd = new SourceDao();
 	String line = "";
     String csvSplitBy = ",";
@@ -98,15 +100,16 @@ public boolean importSource(String filename) throws ClassNotFoundException, SQLE
     	
     	DataSource d = new DataSource();
         connection = d.getConnection();
-        final String query = "INSERT INTO source(source_id ,brightness, longitude, latitude, map_id, source_mapcode, source_x) VALUES (?,?,?,?,4,?,?);";
-        pstatement = connection.prepareStatement(query);
+        //final String query = "INSERT INTO source(source_id ,brightness, longitude, latitude, map_id, source_mapcode, source_x) VALUES (?,?,?,?,4,?,?);";
+        //pstatement = connection.prepareStatement(query);
         System.out.println("importSource: ho fatto la query");
         double longitude;
         double latitude;
+        double fluxValue, fluxError;
         String source_x = null;
         String source_mapcode;
         int sourceId;
-        
+        int MIPSGAL = 3;
         while ((line = br.readLine()) != null) {
 
         	if(rowIndex++ > 11){
@@ -116,6 +119,8 @@ public boolean importSource(String filename) throws ClassNotFoundException, SQLE
         		longitude = Double.parseDouble(vect[1]);
         		latitude = Double.parseDouble(vect[2]);
         		source_mapcode = vect[0];
+        		fluxValue = Double.parseDouble(vect[3]);
+        		fluxError = Double.parseDouble(vect[4]);
         		if(vect.length == 6 )
         			source_x = vect[5];
         		else
@@ -135,24 +140,37 @@ public boolean importSource(String filename) throws ClassNotFoundException, SQLE
         		  			
         		
         			String sql1 = "UPDATE source SET(brightness, longitude, latitude, source_mapcode, source_x) = (?,?,?,?,?) WHERE source_id = ?;";
+        			String sql2 = "UPDATE flux_source SET(value, error) = (?,?) where source_id = ? and band_resolution = ?;";
+
         			pstatement = connection.prepareStatement(sql1);
+        			p2statement = connection.prepareStatement(sql2);
+        			
+        			
+        			p2statement.setDouble(1, fluxValue);
+        			p2statement.setDouble(2, fluxError);
+        			p2statement.setInt(3, sourceId);
+        			p2statement.setDouble(4, 24.0);
+
         			pstatement.setInt(6, sourceId);
         			pstatement.setDouble(1, Double.NaN);
         			pstatement.setDouble(2, longitude);
         			pstatement.setDouble(3, latitude);
         			pstatement.setString(4, source_mapcode);
         			pstatement.setString(5, source_x);
+        			
         			System.out.println("Sono prima dell'update");
         			pstatement.executeUpdate();
+        			p2statement.executeUpdate();
         			System.out.println("Sono dopo l'update");
         			}
         		}
         		else{
         			System.out.println("sto nell'else");
         		
-        			String sql2 = "INSERT INTO source(source_id ,brightness, longitude, latitude, map_id, source_mapcode, source_x) VALUES (?,?,?,?,4,?,?);";
-
-        			pstatement = connection.prepareStatement(sql2);
+        			String sql3 = "INSERT INTO source(source_id ,brightness, longitude, latitude, map_id, source_mapcode, source_x) VALUES (?,?,?,?,?,?,?);";
+        			String sql4 = "INSERT INTO flux_source(band_resolution, source_id, value, error) VALUES (?,?,?,?);";
+        			pstatement = connection.prepareStatement(sql3);
+        			p2statement = connection.prepareStatement(sql4);
         			System.out.println("sto dopo prepare");
 
         			
@@ -161,14 +179,25 @@ public boolean importSource(String filename) throws ClassNotFoundException, SQLE
         			String idQ = "SELECT source_id from source order by source_id DESC;";
                     Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
                     ResultSet result = statement.executeQuery(idQ);
-        			if (result.next())
-        			pstatement.setInt(1, result.getInt("source_id")+ 1);
+        			int sNext = 0;
+                    if (result.next())
+        				sNext = result.getInt("source_id")+ 1;
+        			pstatement.setInt(1, sNext);
         			pstatement.setDouble(2, Double.NaN);
         			pstatement.setDouble(3, longitude);
         			pstatement.setDouble(4, latitude);
-        			pstatement.setString(5, source_mapcode);
-        			pstatement.setString(6, source_x);
+        			pstatement.setInt(5, MIPSGAL);
+        			pstatement.setString(6, source_mapcode);
+        			pstatement.setString(7, source_x);
+        			
+        			p2statement.setDouble(1, 24.0);
+        			p2statement.setInt(2, sNext);
+        			p2statement.setDouble(3, fluxValue);
+        			p2statement.setDouble(4, fluxError);
         			pstatement.executeUpdate();
+        			p2statement.executeUpdate();
+
+        			
         		}
         	}
         }
@@ -184,7 +213,7 @@ public boolean importSource(String filename) throws ClassNotFoundException, SQLE
 
 public static void main(String args[]) throws ClassNotFoundException, SQLException {
 	FileImportDao dao = new FileImportDao();
-	dao.importSource("/Users/Valerio/Desktop/mips.csv");
+	dao.importSource("/home/luca/Scrivania/mips.csv");
 		
 }
 
