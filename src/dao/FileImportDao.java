3d8958157438574550a20fdef8950ad52da5ac58
Roberto Capannelli <roberto.capannelli@gmail.com>
2017-09-13 11:13:43 +0200

    Changed method name for better undestanding

--- src/dao/FileImportDao.java
+++ src/dao/FileImportDao.java
@@ -15,7 +15,7 @@ import java.lang.Object;
 
 public class FileImportDao {
 	
-	public boolean importSatellite(String filename) throws ClassNotFoundException, SQLException {
+	public boolean importClump(String filename) throws ClassNotFoundException, SQLException {
     	
     	Connection connection = null;
 		PreparedStatement statement = null;
@@ -456,7 +456,7 @@ public class FileImportDao {
 
 public static void main(String args[]) throws ClassNotFoundException, SQLException {
 	FileImportDao dao = new FileImportDao();
-	dao.importGlimpse("/Users/Valerio/Desktop/r08.csv");
+	dao.importClump("/Users/robertocapannelli/Desktop/higal.csv");
 		
 }
 
